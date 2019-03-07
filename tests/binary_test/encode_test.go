4e8f09479ce361b3944ce0fb76cfc6928ecaf679
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-08 02:14:20 +0300

    test: each encode decode types

--- tests/binary_test/encode_test.go
+++ tests/binary_test/encode_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestEncode (t *testing.T) {
 	number		:= 510
-	result 		:= binary.Encode(number)
+	result 		:= binary.EncodeInt(number)
 	final 		:= []byte{254, 1}
 
 	if !reflect.DeepEqual(result, final) {
