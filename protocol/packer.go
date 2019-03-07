b54f667fd98183d8dbbf5c8844d79f17218b0710
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-08 02:15:15 +0300

    min: disable unusual code

--- protocol/packer.go
+++ protocol/packer.go
@@ -3,29 +3,29 @@ package protocol
 // упаковщик принимает на вход последовательность байт, тип сообщения,
 // а возвращает пакет, готовый к отправке
 
-func Pack(packet Packet) []byte {
-	packetOffset 	:= 2
-	fieldOffset 	:= 2
-	fieldsLength 	:= 0
+func Pack(packet Packet) /*[]byte*/ {
+	//packetOffset 	:= 2
+	//fieldOffset 	:= 2
+	//fieldsLength 	:= 0
+	//
+	//for i := range packet.Fields {
+	//	fieldsLength += fieldOffset
+	//	fieldsLength += int(packet.Fields[i].FieldSize)
+	//}
+	//
+	//fields := make([]byte, fieldsLength)
+	//for i := range packet.Fields {
+	//	fields = append(fields, packet.Fields[i].FieldID)
+	//	fields = append(fields, packet.Fields[i].FieldSize)
+	//	fields = append(fields, packet.Fields[i].Content...)
+	//}
+	//
+	//buff := make([]byte, fieldsLength + packetOffset + 1)
+	//
+	//buff[0] 			= packet.PacketType
+	//buff[1] 			= packet.PacketSubtype
+	//buff[2:] 			= fields
+	//buff[len(buff)-1] 	= 0x00
 
-	for i := range packet.Fields {
-		fieldsLength += fieldOffset
-		fieldsLength += int(packet.Fields[i].FieldSize)
-	}
-
-	fields := make([]byte, fieldsLength)
-	for i := range packet.Fields {
-		fields = append(fields, packet.Fields[i].FieldID)
-		fields = append(fields, packet.Fields[i].FieldSize)
-		fields = append(fields, packet.Fields[i].Content...)
-	}
-
-	buff := make([]byte, fieldsLength + packetOffset + 1)
-
-	buff[0] 			= packet.PacketType
-	buff[1] 			= packet.PacketSubtype
-	buff[2:] 			= fields
-	buff[len(buff)-1] 	= 0x00
-
-	return buff
+	//return buff
 }
