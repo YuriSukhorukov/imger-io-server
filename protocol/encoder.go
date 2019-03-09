52bc0488987465da58661a6840df2fed6ff09248
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-10 01:30:41 +0300

    fix: correct data fill in slice

--- protocol/encoder.go
+++ protocol/encoder.go
@@ -12,9 +12,9 @@ func EncodePlayer(p model.Player) []Field {
 	b := binary.EncodeInt(place)
 	c := binary.EncodeInt(points)
 
-	f1 := Field{FieldID: 0xAA, FieldSize: 1, Content: a}
-	f2 := Field{FieldID: 0xAA, FieldSize: 2, Content: b}
-	f3 := Field{FieldID: 0xAA, FieldSize: 3, Content: c}
+	f1 := Field{FieldID: 0xAA, FieldSize: byte(len(a)), Content: a}
+	f2 := Field{FieldID: 0xAA, FieldSize: byte(len(b)), Content: b}
+	f3 := Field{FieldID: 0xAA, FieldSize: byte(len(c)), Content: c}
 
 	fields := []Field{f1, f2, f3}
 
@@ -25,8 +25,8 @@ func EncodePoint(p model.Point) []Field {
 	a := []byte{byte(p.X)}
 	b := []byte{byte(p.Y)}
 
-	field1 := Field{FieldID: 0xAA, FieldSize: 1, Content: a}
-	field2 := Field{FieldID: 0xAA, FieldSize: 2, Content: b}
+	field1 := Field{FieldID: 0xAA, FieldSize: byte(len(a)), Content: a}
+	field2 := Field{FieldID: 0xAA, FieldSize: byte(len(b)), Content: b}
 
 	fields := []Field{field1, field2}
 
