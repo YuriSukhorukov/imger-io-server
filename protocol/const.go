99ec71b96eeb57cbcd28eee5c98a05e70bd1acf8
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-10 01:29:42 +0300

    min: additional events for player entity

--- protocol/const.go
+++ protocol/const.go
@@ -7,5 +7,8 @@ const LINE_DOT 			byte 	= 	 0x04
 const LINE_COLOR		byte	= 	 0x05
 const LINE_WEIGHT		byte 	= 	 0x06
 
+const ENTITY			byte	=	 0x07
+const PLAYER 			byte	=	 0x08
+
 const END_FIELD			byte 	= 	 0x00
 const END_PACKET		byte	=	 0x00
