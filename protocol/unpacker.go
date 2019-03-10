e8119a2388e73eb56e124e0a7da1b1503857a6b2
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-10 17:03:24 +0300

    min: packet unpacker

--- protocol/unpacker.go
+++ protocol/unpacker.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "fmt"
+
 // распаковщик принимает на вход последодвательность байт, коллекцию типов сообщений
 // а возвращает пакет, готовый к использованию
 
@@ -11,12 +13,17 @@ func Unpack(buff []byte) Packet {
 
 	var fieldID 	byte
 	var fieldSize 	byte
-	var content		[]byte
+	fmt.Printf("buff size: %d\n", len(buff))
 
-	for i := 2; i < len(buff) - 1; i += int(fieldSize) {
+	for i := 2; i + 2 + int(fieldSize) < len(buff); i += 2 + int(fieldSize) {
+		fmt.Printf("index: %d\n", i)
 		fieldID 	= buff[i]
-		fieldSize 	= buff[i+1]
-		content		= buff[i + 2:fieldSize]
+		fieldSize 	= buff[i + 1]
+		fmt.Printf("field id: %d \n", int(fieldID))
+		fmt.Printf("field size: %d \n", int(fieldSize))
+		fmt.Printf("slice from: %d to %d\n", i+2, i+2+int(fieldSize))
+		fmt.Printf("content: %v \n", buff[i+2:i+2+int(fieldSize)])
+		content		:= buff[i+2:i+2+int(fieldSize)]
 		fields 		= append(fields, Field{FieldID: fieldID, FieldSize: fieldSize, Content: content})
 	}
 
@@ -25,6 +32,8 @@ func Unpack(buff []byte) Packet {
 		PacketSubtype: 	packetSubtype,
 		Fields: 		fields,
 	}
+
+	fmt.Printf("%v: \n", packet)
 
 	return packet
 }
