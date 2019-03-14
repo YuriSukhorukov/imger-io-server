d5ec6184f9d7b4cc46b874967f65b79f2ba0a0a2
Yuri Sukhorukov <33977436+YuriSukhorukov@users.noreply.github.com>
2019-03-14 15:34:20 +0300

    refactor: code style

--- protocol/unpacker.go
+++ protocol/unpacker.go
@@ -4,29 +4,24 @@ package protocol
 // а возвращает пакет, готовый к использованию
 
 func Unpack(buff []byte) Packet {
-	packetType 		:= buff[0]
+	packetType 	:= buff[0]
 	packetSubtype 	:= buff[1]
-
-	fields 			:= make([]Field, 0)
-
+	fields 		:= make([]Field, 0)
+	
 	var fieldID 	byte
 	var fieldSize 	byte
-
 	for i := 2; i < len(buff) - 1; i += 2 + int(buff[i+1])  {
-		fieldID = buff[i]
-		fieldSize = buff[i+1]
-		contentStartIndex := i + 2
-		contentEndIndex := contentStartIndex + int(fieldSize)
-
-		content := buff[contentStartIndex:contentEndIndex]
-		fields = append(fields, Field{FieldID: fieldID, FieldSize: fieldSize, Content: content})
+		fieldID 		= buff[i]
+		fieldSize 		= buff[i+1]
+		contentStartIndex 	:= i + 2
+		contentEndIndex 	:= contentStartIndex + int(fieldSize)
+		content 		:= buff[contentStartIndex:contentEndIndex]
+		fields 			= append(fields, Field{FieldID: fieldID, FieldSize: fieldSize, Content: content})
 	}
-
 	packet := Packet{
 		PacketType: 	packetType,
 		PacketSubtype: 	packetSubtype,
-		Fields: 		fields,
+		Fields: 	fields,
 	}
-
 	return packet
 }
