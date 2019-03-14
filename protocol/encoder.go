800a4da49623f21d10a300ed1490f1f71cf18fd8
Yuri Sukhorukov <33977436+YuriSukhorukov@users.noreply.github.com>
2019-03-14 15:26:39 +0300

    Update encoder.go
    refactor: naming, code style

--- protocol/encoder.go
+++ protocol/encoder.go
@@ -6,33 +6,33 @@ import (
 import "../binary"
 
 func EncodePlayer(p model.Player) []Field {
-	name 		:= binary.EncodeString(p.Name)
-	place 		:= binary.EncodeInt(int(p.Place))
-	points		:= binary.EncodeInt(int(p.Points))
-	f1 			:= Field{FieldID: 0xAA, FieldSize: byte(len(name)), Content: name}
-	f2 			:= Field{FieldID: 0xAA, FieldSize: byte(len(place)), Content: place}
-	f3 			:= Field{FieldID: 0xAA, FieldSize: byte(len(points)), Content: points}
+	b1 		:= binary.EncodeString(p.Name)
+	b2 		:= binary.EncodeInt(int(p.Place))
+	b3		:= binary.EncodeInt(int(p.Points))
+	f1 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b1)), Content: b1}
+	f2 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b2)), Content: b2}
+	f3 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b3)), Content: b3}
 	return 		[]Field{f1, f2, f3}
 }
 
 func EncodePoint(p model.Point) []Field {
-	x 			:= binary.EncodeInt(int(p.X))
-	y 			:= binary.EncodeInt(int(p.Y))
-	f1 			:= Field{FieldID: 0xAA, FieldSize: byte(len(x)), Content: x}
-	f2 			:= Field{FieldID: 0xAA, FieldSize: byte(len(y)), Content: y}
-	return 	    []Field{f1, f2}
+	b1 		:= binary.EncodeInt(int(p.X))
+	b2 		:= binary.EncodeInt(int(p.Y))
+	f1 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b1)), Content: b1}
+	f2 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b2)), Content: b2}
+	return 	    	[]Field{f1, f2}
 }
 
 func EncodeMessage(m model.Message) []Field {
-	name 		:= binary.EncodeString(m.Name)
-	text 		:= binary.EncodeString(m.Text)
-	f1 			:= Field{FieldID: 0xAA, FieldSize: byte(len(name)), Content: name}
-	f2 			:= Field{FieldID: 0xAA, FieldSize: byte(len(text)), Content: text}
-	return 	    []Field{f1, f2}
+	b1 		:= binary.EncodeString(m.Name)
+	b2 		:= binary.EncodeString(m.Text)
+	f1 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b1)), Content: b1}
+	f2 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b2)), Content: b2}
+	return 	    	[]Field{f1, f2}
 }
 
 func EncodeTime(t model.Time) []Field {
-	seconds 	:= binary.EncodeInt(int(t.Seconds))
-	f1 			:= Field{FieldID: 0xAA, FieldSize: byte(len(seconds)), Content: seconds}
-	return 	    []Field{f1}
+	b1 		:= binary.EncodeInt(int(t.Seconds))
+	f1 		:= Field{FieldID: 0xAA, FieldSize: byte(len(b1)), Content: b1}
+	return 	    	[]Field{f1}
 }
