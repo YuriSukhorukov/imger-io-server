75cd8bb9d7bf06168d998ed0e253d1504ba72697
Yuri Sukhorukov <33977436+YuriSukhorukov@users.noreply.github.com>
2019-03-14 15:43:35 +0300

    refactor: code style

--- binary/decoder.go
+++ binary/decoder.go
@@ -1,11 +1,11 @@
 package binary
 
 func DecodeInt(buff []byte) int {
-	number := 0
+	number  := 0
 	for i := len(buff) - 1; i >= 0; i-- {
 		number |= int(buff[i]) << uint(i * 8)
 	}
-	return number
+	return  number
 }
 
 func DecodeString(buff []byte) string {
