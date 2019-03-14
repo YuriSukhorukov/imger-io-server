4f8fbd1240c675fb8bbfbee7e25e982bced4738a
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-14 22:12:21 +0300

    refactor: code style

--- protocol/decoder.go
+++ protocol/decoder.go
@@ -6,10 +6,10 @@ import (
 )
 
 func DecodePlayer(f []Field) model.Player {
-	name 		:= binary.DecodeString(f[0].Content)
-	place 		:= binary.DecodeInt(f[1].Content)
-	points 		:= binary.DecodeInt(f[2].Content)
-	return 		model.Player{Name: name, Place: uint8(place), Points: uint8(points)}
+	name        := binary.DecodeString(f[0].Content)
+	place       := binary.DecodeInt(f[1].Content)
+	points      := binary.DecodeInt(f[2].Content)
+	return      model.Player{Name: name, Place: uint8(place), Points: uint8(points)}
 }
 
 func DecodePoint(f []Field) model.Point {
@@ -19,12 +19,12 @@ func DecodePoint(f []Field) model.Point {
 }
 
 func DecodeMessage(f []Field) model.Message {
-	name 		:= binary.DecodeString(f[0].Content)
-	text 		:= binary.DecodeString(f[1].Content)
-	return 		model.Message{Name: name, Text: text}
+	name        := binary.DecodeString(f[0].Content)
+	text        := binary.DecodeString(f[1].Content)
+	return      model.Message{Name: name, Text: text}
 }
 
 func DecodeTime(f []Field) model.Time {
-	seconds 	:= binary.DecodeInt(f[0].Content)
-	return 		model.Time{Seconds: uint(seconds)}
+	seconds     := binary.DecodeInt(f[0].Content)
+	return      model.Time{Seconds: uint(seconds)}
 }
