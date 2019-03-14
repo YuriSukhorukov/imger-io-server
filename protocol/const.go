81765d664646e263ad4ce74d679497747789e0f2
Yuri Sukhorukov <33977436+YuriSukhorukov@users.noreply.github.com>
2019-03-14 15:37:32 +0300

    refactor: remove excess, code style

--- protocol/const.go
+++ protocol/const.go
@@ -1,14 +1,13 @@
 package protocol
 
-const CANVAS 			byte 	= 	 0x01
-const LINE_BEGINNING 	byte 	= 	 0x02
-const LINE_ENDING 		byte 	= 	 0x03
-const LINE_DOT 			byte 	= 	 0x04
-const LINE_COLOR		byte	= 	 0x05
-const LINE_WEIGHT		byte 	= 	 0x06
+const CANVAS 			    byte = 0x01
+const LINE_BEGINNING 	byte = 0x02
+const LINE_ENDING 		byte = 0x03
+const LINE_DOT 			  byte = 0x04
+const LINE_COLOR		  byte = 0x05
+const LINE_WEIGHT		  byte = 0x06
 
-const ENTITY			byte	=	 0x07
-const PLAYER 			byte	=	 0x08
+const ENTITY			    byte = 0x07
+const PLAYER 			    byte = 0x08
 
-const END_FIELD			byte 	= 	 0x00
-const END_PACKET		byte	=	 0x00
+const END_PACKET		  byte = 0x00
