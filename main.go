56eb615999b67bb98a0ade8cd37d530a0b8c4d00
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2019-03-31 04:10:12 +0300

    min: remove excess

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"net"
-)
-import "os"
-import "fmt"
-import "./protocol"
-
+import "./network"
 // требуется только ниже для обработки примера
 
 const CONN_HOST = "localhost"
@@ -16,45 +9,5 @@ const CONN_TYPE = "tcp"
 const CONN_URL  = CONN_HOST + ":" + CONN_PORT
 
 func main() {
-	fmt.Println("Launching server...")
-	// Устанавливаем прослушивание порта
-	ln, err := net.Listen(CONN_TYPE, CONN_PORT)
-
-	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
-	}
-
-	// Close the listener when this application closes
-	defer ln.Close()
-
-	// Открываем порт
-	conn, _ := ln.Accept()
-	defer conn.Close()
-	// Запускаем цикл
-	for {
-		fmt.Printf("\n--->>>\n")
-		// Будем прослушивать все сообщения разделенные \n
-		//message, _ := bufio.NewReader(conn).ReadString('\n')
-		//data := make([]byte, 1024)
-		//message, _ := conn.Read(data)
-		//s := string(data[:message])
-
-		buff, err := bufio.NewReader(conn).ReadBytes(0x00)
-		if err != nil {
-			return
-		}
-
-		packet := protocol.Unpack(buff)
-		if packet.PacketType == protocol.ENTITY &&
-			packet.PacketSubtype == protocol.PLAYER {
-			player := protocol.DecodePlayer(packet.Fields)
-			fmt.Printf("player from packet:  %v\n", player)
-			fmt.Printf("received packet:     %v\n", packet)
-		}
-
-		//// Отправить новую строку обратно клиенту
-		//conn.Write([]byte(s))
-		//fmt.Printf("\n---<<<")
-	}
+	network.Open()
 }
